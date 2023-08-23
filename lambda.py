167b6f6e0b158e0a15df8b00b92f496d34d51ae0
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-23 12:14:58 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
 
   response = requests.post('https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data',
                              headers=request_headers,
-                             data=json.dumps(request_body))
+                             data=jrequest_body)
   
   
  
